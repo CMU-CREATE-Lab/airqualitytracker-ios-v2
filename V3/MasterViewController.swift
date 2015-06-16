bc16b1e5964c93067e50f84ce527f1fe703542b5
Mohak Nahta <mohaknahta@Mohaks-Air-2.wv.cc.cmu.edu>
2015-06-16 16:16:40 -0400

    successfully added and linked the settings page

--- V3/MasterViewController.swift
+++ V3/MasterViewController.swift
@@ -29,7 +29,7 @@ class MasterViewController: UITableViewController, CLLocationManagerDelegate {
         getCurrentLocality()
         getCurrentAirQuality()
         super.viewDidLoad()
-        self.navigationItem.leftBarButtonItem = self.editButtonItem()
+//        self.navigationItem.leftBarButtonItem = self.editButtonItem()
     }
     
     //MARK: - gets current coordinates
