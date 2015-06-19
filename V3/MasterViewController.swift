51794322332ea35ea2af2a95fa3ff22e8dd09849
Mohak Nahta <mohaknahta@Mohaks-Air-2.wv.cc.cmu.edu>
2015-06-19 16:28:00 -0400

    aqi for MVC working.

--- V3/MasterViewController.swift
+++ V3/MasterViewController.swift
@@ -18,6 +18,7 @@ class MasterViewController: UITableViewController, CLLocationManagerDelegate {
     var longitude: Double = 0
     
     var currentLocation: String = ""
+    var stationID: Int = 0
     var airQuality: Int = 0
     
     var currentTemperature: String  = ""
@@ -41,6 +42,7 @@ class MasterViewController: UITableViewController, CLLocationManagerDelegate {
         getCurrentLocality()
         getCurrentWeatherData()
         getCurrentAirQuality()
+//        getM()
         tableView.delegate = self
         tableView.dataSource = self
         super.viewDidLoad()
@@ -127,7 +129,6 @@ class MasterViewController: UITableViewController, CLLocationManagerDelegate {
         var currentLatitude = latitude
         var currentLongitude = longitude
         
-        
         var currentDate = NSDate()
         var currentDateInSeconds = currentDate.timeIntervalSince1970
         var last24Hours = currentDateInSeconds - (60 * 60 * 24)
@@ -145,18 +146,80 @@ class MasterViewController: UITableViewController, CLLocationManagerDelegate {
             NSJSONSerialization.JSONObjectWithData(dataObject!, options: nil, error: nil) as! NSDictionary //casting
             
             let currentAir = CurrentAirQuality(airQualityDictionary: airQualityDictionary, currentLatitude: currentLatitude, currentLongitude: currentLongitude)
-            
-            self.airQuality = currentAir.closestStationID
-            println("sending data to struct from MVC")
-            let location = LocationForList(description: "Current Location", AQI: "\(self.airQuality)", lat: self.latitude, long: self.longitude, temp: self.currentTemperature, Oz: self.currentOzone)
-            LocationStore.sharedInstance.add(location)
-            println("##################################")
+      
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                self.stationID = currentAir.closestStationID
+                self.getAQI()
+            })
 
         })
-        
         downloadTask.resume()
+
     }
-    
+        
+        func getAQI() -> Void {
+            var placesTask = NSURLSessionDataTask()
+            var session: NSURLSession {
+                return NSURLSession.sharedSession()
+            }
+            println("entering AQI most recent value methods and call...")
+            println("station ID is \(self.stationID)")
+            var AQIURL = "https://esdr.cmucreatelab.org/api/v1/feeds/\(self.stationID)/most-recent"
+            
+            var AQIURLString = AQIURL.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
+            
+            if placesTask.taskIdentifier > 0 && placesTask.state == .Running {
+                placesTask.cancel()
+            }
+
+            UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+            placesTask = session.dataTaskWithURL(NSURL(string: AQIURLString)!) {data, response, error in
+                UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+
+                if let dataObject = NSJSONSerialization.JSONObjectWithData(data, options:nil, error:nil) as? NSDictionary{
+                    
+                    if let data = dataObject["data"] as? NSDictionary {
+                        println("first if conditional passed...")
+                        if let channels = data["channels"] as? NSDictionary {
+                            println("second if conditional passed...")
+                            if let val: AnyObject = channels["PM2_5"]{
+                                println("in PM2.5...")
+                                self.getMostRecentValue(channels, identifier: "PM2_5")
+                            }
+                            
+                            else if let val: AnyObject = channels["PM25B_UG_M3"]{
+                                self.getMostRecentValue(channels, identifier: "PM25B_UG_M3")
+                            }
+                            
+                            else if let val: AnyObject = channels["PM25_FL_PERCENT"]{
+                                self.getMostRecentValue(channels, identifier: "PM25_FL_PERCENT")
+                            }
+                            
+                            else if let val: AnyObject = channels["PM25_UG_M3"]{
+                                self.getMostRecentValue(channels, identifier: "PM25_UG_M3")
+                            }
+                        }
+                        
+                    }
+                }
+                dispatch_async(dispatch_get_main_queue()) {
+                    println("sending data to struct from MVC")
+                    let location = LocationForList(description: "Current Location", AQI: "\(self.airQuality)", lat: self.latitude, long: self.longitude, temp: self.currentTemperature, Oz: self.currentOzone)
+                    LocationStore.sharedInstance.add(location)
+                    println("##################################")
+                }
+            }
+            placesTask.resume()
+        }
+
+func getMostRecentValue(channelsDict: NSDictionary, identifier: String){
+    println("in getMostRecentValue()...")
+    let temp: NSDictionary = channelsDict[identifier] as! NSDictionary
+    let mRDS = temp["mostRecentDataSample"] as! NSDictionary
+    self.airQuality  = mRDS["value"] as! Int
+    println("air quality is \(self.airQuality)")
+}
+
     func createBoundingBox(currentLatitude: Double, currentLongitude: Double) -> (Double, Double, Double, Double){
         var distance = 10 //in kilometers
         var radius = 6371 //in km
@@ -199,42 +262,28 @@ class MasterViewController: UITableViewController, CLLocationManagerDelegate {
                 if (SettingsViewController.variables.unit == true){
                     temperatureSymbol = "\u{00B0} F" //symbol for degree F
                 }
-                    
                 else{
                     temperatureSymbol = "\u{00B0} C" //symbol for degree C
                 }
-                
-                
                 dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                    
                     self.currentTemperature = "\(currentWeather.temperature)" + "\(temperatureSymbol)"
-                    
                     self.currentOzone = "\(currentWeather.ozone)"
                 })
-                
             }
-                
             else {
                 let issue = UIAlertController(title: "Error", message: "Error in connection", preferredStyle: .Alert)
-                
                 let okIssue = UIAlertAction(title: "OK", style: .Default, handler: nil)
                 issue.addAction(okIssue)
-                
                 let cancelIssue = UIAlertAction(title: "Cancel", style: .Default, handler: nil)
                 issue.addAction(cancelIssue)
-                
                 self.presentViewController(issue, animated: true, completion: nil)
-                
             }
-            
-        })
-        
-        
+            })
         
         downloadTask.resume()
-    }
+}
 
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
